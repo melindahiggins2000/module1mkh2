7a8c6b1b3ab5a6b3a073dc00da97864bf3a7cecf
melindahiggins2000 <melinda.higgins@emory.edu>
2015-07-06 14:57:18 -0400

    more downloading

--- downloadingdata.R
+++ downloadingdata.R
@@ -38,5 +38,44 @@ cameras2 = read.csv("cameras.csv")
 
 head(cameras)
 
+# look at readr
+
+install.packages("readr")
+library(readr)
+cameras3 <- read_csv("cameras.csv")
+
+system.time(read.csv("cameras.csv"))
+system.time(read_csv("cameras.csv"))
+
+dat <- matrix(rnorm(100*1000),nrow=1000)
+write.csv(dat,file="dat.csv")
+system.time(read.csv("dat.csv"))
+system.time(read_csv("dat.csv"))
+
+# > system.time(read.csv("dat.csv"))
+# user  system elapsed 
+# 0.15    0.00    0.14 
+# > system.time(read_csv("dat.csv"))
+# user  system elapsed 
+# 0.03    0.00    0.03 
+
+fileUrl2 <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+
+download(fileUrl2,
+         destfile="cameras.xlsx")
+list.files()
+dateDownloaded <- date()
+dateDownloaded
+
+# downloaded manually
+# Baltimore_Fixed_Speed_Cameras.xlsx
+
+install.packages("readxl")
+library(readxl)
+cameras4 <- read_excel("Baltimore_Fixed_Speed_Cameras.xlsx", sheet=1)
+
+
+
+
 
 
